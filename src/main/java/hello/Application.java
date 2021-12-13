b76784e51d6c80591418f095eb39150cca76fa9a
“ptr-z” <piotr.zajac@allegro.pl>
2021-12-13 11:02:23 +0000

    init

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,128 @@
+package hello;
+
+import org.slf4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Application.class);
+    static int counter;
+
+    static class Self {
+        public String href;
+    }
+
+    static class Links {
+        public Self self;
+    }
+
+    static class PlayerState {
+        public Integer x;
+        public Integer y;
+        public String direction;
+        public Boolean wasHit;
+        public Integer score;
+    }
+
+    static class Arena {
+        public List<Integer> dims;
+        public Map<String, PlayerState> state;
+    }
+
+    static class ArenaUpdate {
+        public Links _links;
+        public Arena arena;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.initDirectFieldAccess();
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Let the battle begin!";
+    }
+
+    @PostMapping("/**")
+    public String index(@RequestBody ArenaUpdate arenaUpdate) {
+        System.out.println(arenaUpdate);
+        String[] commands = new String[]{"F", "R", "L"};
+        int i = new Random().nextInt(3);
+
+        String self = arenaUpdate._links.self.href;
+        PlayerState me = arenaUpdate.arena.state.get(self);
+        List<PlayerState> others = getOthers(arenaUpdate, self);
+
+        if (me.wasHit) {
+            log.info("I was hit");
+            return "F";
+        }
+
+        if (isAnyoneInFrontOfMe(me, others)) {
+            return "T";
+        } else {
+
+            if (me.wasHit) {
+                log.info("Fleeing");
+                return "F";
+            }
+            counter++;
+            if (counter > 2) {
+                counter = 0;
+                return "F";
+            }
+            return "L";
+        }
+    }
+
+    private List<PlayerState> getOthers(ArenaUpdate arenaUpdate, String self) {
+        return arenaUpdate.arena.state.entrySet().stream()
+                .filter(e -> !e.getKey().equals(self))
+                .map(Map.Entry::getValue)
+                .collect(Collectors.toList());
+    }
+
+    private boolean isAnyoneInFrontOfMe(PlayerState me, List<PlayerState> others) {
+        Set<Integer> oy = others.stream().map(e -> e.y).collect(Collectors.toSet());
+        Set<Integer> ox = others.stream().map(e -> e.x).collect(Collectors.toSet());
+
+        switch (me.direction) {
+            case "N":
+                log.info("N - found someone");
+                return oy.stream().anyMatch(y -> me.y - y <= 3);
+            case "W":
+                log.info("W - found someone");
+                return ox.stream().anyMatch(x -> me.x - x <= 3);
+            case "S":
+                log.info("S - found someone");
+                return oy.stream().anyMatch(y -> y - me.y <= 3);
+            case "E":
+                log.info("E - found someone");
+                return ox.stream().anyMatch(x -> x - me.x <= 3);
+            default:
+                System.out.println("THERE IS NO CASE");
+                return false;
+        }
+    }
+
+}
